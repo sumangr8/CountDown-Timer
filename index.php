13d25f98620509379f0a0e2918bf01a3e4ef8b12
sumangr8 <30518152+sumangr8@users.noreply.github.com>
2020-02-27 21:06:31 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+	function timeout()
+	{
+		var hours=Math.floor(timeLeft/3600);
+		var minute=Math.floor((timeLeft-(hours*60*60)-30)/60);
+		var second=timeLeft%60;
+		var hrs=checktime(hours);
+		var mint=checktime(minute);
+		var sec=checktime(second);
+		if(timeLeft<=0)
+		{
+			clearTimeout(tm);
+			document.getElementById("form1").submit();
+		}
+		else
+		{
+
+			document.getElementById("time").innerHTML=hrs+":"+mint+":"+sec;
+		}
+		timeLeft--;
+		var tm= setTimeout(function(){timeout()},1000);
+	}
+	function checktime(msg)
+	{
+		if(msg<10)
+		{
+			msg="0"+msg;
+		}
+		return msg;
+	}
+	</script>
+</head>
+<body onload="timeout()">
+<div class="container">
+	<script type="text/javascript">
+		  var timeLeft=2*60*60;
+	</script>
+
+<div id="time"style="float:right">timeout</div></h2>
+</div>
+
+</body>
+</html>
